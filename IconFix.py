4fde6e5362c56a2e24c99696040bd6a71eea67f2
Einck <91471683+Einck0@users.noreply.github.com>
2023-07-09 12:27:10 +0800

    Add files via upload

--- IconFix.py
+++ IconFix.py
@@ -7,7 +7,7 @@ alphabet_upper = string.ascii_uppercase
 dir_dict={}
 #获取Steam安装目录
 mode = '1'
-if input('是否使用默认Steam安装目录？(Y/N)') in{'Y', 'y'}:
+if input('是否使用默认Steam安装目录？(Y/y)') in{'Y', 'y'}:
     mode = '0'
     address_list = [r':\SteamLibrary\steamapps\common'+'\\',r':\Steam\steamapps\common'+'\\',r':\Program Files (x86)\Steam\steamapps\common'+'\\',r':\Program Files\Steam\steamapps\common'+'\\']
     for address in address_list:    
@@ -42,7 +42,7 @@ try:
             key, value = line.split('=', 1)
             cookies[key] = value
 except ValueError:
-    os._exit(1)
+    exit
 #计算相似度
 def compare_name(dir_list,name_list):
     count=0
@@ -67,6 +67,7 @@ def get_file_list(folder_path,type):
     #将文件名按照长度顺序排序
     url_files.sort(key=lambda x:len(x))
     return url_files
+
 # 获取图标
 def find_icon(id):
     url=weburl+id
@@ -88,48 +89,74 @@ def find_icon(id):
     elif get_file_list(dir_dict[name],'exe')!=[]:
         icon=dir_dict[name]+get_file_list(dir_dict[name],'exe')[0]
     return icon
+
 # 替换图标
-def replace_icon(url_files,folder_path):
+def replace_icon(file_name,folder_path):
+    if file_name == '0':
+        return
     # 打开文件并读取内容
-    for file_name in url_files:
-        with open(folder_path+"\\"+file_name, 'r') as file:
-            content = file.read()
-        # 获取图标路径
-            pattern=r'URL=steam://rungameid/(.*)'
-            id=re.findall(pattern,content)
-            icon=find_icon(id[0])
-            if icon==None:
-                continue
-            icon=icon.replace('\\','\\\\')
-            content=re.sub(r'IconIndex=.*',r'IconIndex=0',content)
-            content=re.sub(r'IconFile=.*',r'IconFile='+icon,content)
-            print(file_name+' '+icon)
+    with open(folder_path+"\\"+file_name, 'r') as file:
+        content = file.read()
+    # 获取图标路径
+        pattern=r'URL=steam://rungameid/(.*)'
+        id=re.findall(pattern,content)
+        icon=find_icon(id[0])
+        if icon==None:
+            print(file_name+' 未找到图标')
+            return
+        icon=icon.replace('\\','\\\\')
+        content=re.sub(r'IconIndex=.*',r'IconIndex=0',content)
+        content=re.sub(r'IconFile=.*',r'IconFile='+icon,content)
+        print(file_name+' '+icon)
     # 替换文本
     #content = content.replace('old_text', 'new_text')
 # 打开文件以写入模式，并将修改后的内容写入文件
-            try:
-                with open(folder_path+'\\'+file_name, 'w') as file:
-                    file.write(content)
-                file.close()
-            except PermissionError:
-                print(file_name+' Permission Denied')
-                continue
+        try:
+            with open(folder_path+'\\'+file_name, 'w') as file:
+                file.write(content)
+            file.close()
+        except PermissionError:
+            print(file_name+' Permission Denied')
+            print('\n请关闭文件后，右键管理员权限再次运行程序\n')
+            os.system('pause')
+            os._exit(1)
 
 def main():
     # 获取文件
     folder_path = os.getcwd()
+    public_path=r'C:\Users\Public\Desktop'
     print('当前文件夹为：'+folder_path)
     url_files = get_file_list(folder_path,'url')
+    count=len(url_files)
+    url_files+= get_file_list(public_path,'url')
+    #选择需要修改的文件
+    print('找到'+str(len(url_files))+'个Steam快捷方式')
+    print('0'+':全部')
+    for i in range(len(url_files)):
+        print(str(i+1)+':'+url_files[i],end='   ')
+        if (i+1)%5==0:
+            print('\n',end='')
+    print('\n请选择需要修改的文件(用空格隔开)：')
+    num_list=list(map(int,input().split()))
+    if 0 not in num_list:
+        for i in range(len(url_files)):
+            if i+1 not in num_list:
+                url_files[i]='0'
     # 替换图标
-    print('替换图标中...')
-    replace_icon(url_files,folder_path)
+    i=0
+    while i < count:
+        replace_icon(url_files[i],folder_path)
+        i+=1
     print('当前文件夹已替换完成，尝试替换公共桌面快捷方式图标...')
-    url_files = get_file_list(r'C:\Users\Public\Desktop','url')
-    replace_icon(url_files,r'C:\Users\Public\Desktop')
-    print('\n替换完成,过一段时间即可生效\n')
+    while i < len(url_files):
+        replace_icon(url_files[i],public_path)
+        i+=1
+
+    print('\n替换完成,WIN11过一段时间才可生效\n')
     print('如图标错误，请再次运行程序手动输入Steam安装目录\n如仍然错误,请手动修改快捷方式图标\n')
     print('如有其他问题，请联系作者\n')
-    print(r'Github:https://github.com/Einck0/IconFix\n')
+    print(r'Github:https://github.com/Einck0/IconFix')
     os.system('pause')
+
 if __name__ == "__main__":
     main()
